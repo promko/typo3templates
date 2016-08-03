30fc9f8749896669bc9af5df70efe08fbf1bcb5f
promko <pryshlyak@gmail.com>
2016-08-03 20:30:07 +0300

    Simple WORKING ViewHelpers

--- htdocs/typo3conf/ext/simpleblog/Classes/ViewHelpers/DummyTextViewHelper.php
+++ htdocs/typo3conf/ext/simpleblog/Classes/ViewHelpers/DummyTextViewHelper.php
@@ -1,26 +1,25 @@
 <?php
 
-namespacePluswerk\Simpleblog\ViewHelpers;
+namespace Pluswerk\Simpleblog\ViewHelpers;
 
-class DummyTextViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class DummyTextViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper 
+{
 
-    public function initializeArguments() {
+    public function initializeArguments() 
+            {
         $this->registerArgument('length', 'integer', 'This is the length of the dummytext', FALSE);
     }
 
-    /**
-     * @param int $length
-     * @return string
-     */
-    public function render($length = 100) {
-        $length = ($this->arguments['length']) ?: 100;
-        $dummytext = ($this->renderChildren()) ?: 'Lorem ipsum dolor sit amet. ';
+    public function render() {
+        $length = ($this->arguments['length']) ? : 100;
+        $dummytext = ($this->renderChildren()) ? : 'Lorem ipsum dolor sit amet. ';
         $len = strlen($dummytext);
         $repeat = ceil($length / $len);
-        for ($i=0; $i <= $repeat; $i++) {
-            $dummytext_neu .= $dummytext;
-        }
-        //$dummytext_neu = substr(str_repeat($dummytext, $repeat), 0, $length);
+//        for ($i = 0; $i <= $repeat; $i++) {
+//            $dummytext_neu .= $dummytext;
+//        }
+//         $dummytext_neu = "FFFFFFFFFFFFFFFFFF";
+        $dummytext_neu = substr(str_repeat($dummytext, $repeat), 0, $length);
         return $dummytext_neu;
     }
 
