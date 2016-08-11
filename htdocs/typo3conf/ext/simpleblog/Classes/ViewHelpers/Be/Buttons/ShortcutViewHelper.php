87f46b1850fc813bc589463dd513e5a633a88355
promko <pryshlyak@gmail.com>
2016-08-11 23:25:00 +0300

    BackEnd Module

---
+++ htdocs/typo3conf/ext/simpleblog/Classes/ViewHelpers/Be/Buttons/ShortcutViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+namespace Pluswerk\Simpleblog\ViewHelpers\Be\Buttons;
+
+use TYPO3\CMS\Backend\Template\DocumentTemplate;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+use TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBackendViewHelper;
+
+/**
+ * Wrapper for f:be.buttons.shortcut
+ * Adapts HTML for 7.6 ModuleTemplate API
+ */
+class ShortcutViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Be\Buttons\ShortcutViewHelper {
+
+    /**
+     * @param array $arguments
+     * @param callable $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext) {
+        $getVars = $arguments['getVars'];
+        $setVars = $arguments['setVars'];
+        $mayMakeShortcut = $GLOBALS['BE_USER']->mayMakeShortcut();
+        if ($mayMakeShortcut) {
+            $doc = GeneralUtility::makeInstance(DocumentTemplate::class);
+            $currentRequest = $renderingContext->getControllerContext()->getRequest();
+            $extensionName = $currentRequest->getControllerExtensionName();
+            $moduleName = $currentRequest->getPluginName();
+            if (count($getVars) === 0) {
+                $modulePrefix = strtolower('tx_' . $extensionName . '_' . $moduleName);
+                $getVars = array('id', 'M', $modulePrefix);
+            }
+            $getList = implode(',', $getVars);
+            $setList = implode(',', $setVars);
+            return $doc->makeShortcutIcon($getList, $setList, $moduleName, '', 'btn btn-default btn-sm');
+        }
+        return '';
+    }
+}
--- htdocs/typo3conf/ext/simpleblog/ext_tables.php
+++ htdocs/typo3conf/ext/simpleblog/ext_tables.php
@@ -1,12 +1,11 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Pluswerk.' . $_EXTKEY,
-	'Bloglisting',
-	'Simpleblog-Bloglisting'
+        'Pluswerk.' . $_EXTKEY, 'Bloglisting', 'Simpleblog-Bloglisting'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Simple Blog Extension');
@@ -25,3 +24,23 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_simpleblog_domain_model_tag', 'EXT:simpleblog/Resources/Private/Language/locallang_csh_tx_simpleblog_domain_model_tag.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_simpleblog_domain_model_tag');
+
+if (TYPO3_MODE === 'BE') {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Pluswerk.' . $_EXTKEY, 
+            'system', 
+            'SimpleblogAdmin', 
+            'top', 
+            array('Comment' => 'index,list,delete,test'), 
+            array(
+                'access' => 'admin',
+                'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+                'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf'
+            )
+    );
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+'_MOD_system_SimpleblogSimpleblogadmin',
+'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh.xlf'
+);
